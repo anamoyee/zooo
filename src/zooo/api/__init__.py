a3fb2efa016b39de832d98edc9c34c91f65bf98d
anamoyee <thecreatorrr333@gmail.com>
2025-07-09 18:53:19 +0200

    fix git file endings shit i tihnk

--- src/zooo/api/__init__.py
+++ src/zooo/api/__init__.py
@@ -1,6 +1,6 @@
 from aiolimiter import AsyncLimiter
 from tcrutils.result import Result2 as Result
 
-from . import error, utils, type
+from . import error, type, utils
 from .client import Client
 from .type import ListedProfile, NPCProfileInfo, ProfileID, ProfileInfo, UserInfo, Zoo
